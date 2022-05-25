df1c6b35b399056c0f18c6b455d5603f78b326fd
Lucas Gomes <jatinhahxdez@gmail.com>
2022-05-25 19:56:18 -0300

    feat: add deposit method

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ app.use(express.json());
 
 const customers = []
 
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer) {
+        return response.status(400).json({ error : "Customer Not Found"});
+    }
+
+    request.customer = customer
+
+    return next();
+}
+
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
 
@@ -28,17 +42,30 @@ app.post("/account", (request, response) => {
     return response.send(201).send();
 });
 
-app.get("/statement", (request, response) =>{
-    const { cpf } = request.headers;
-
-    const customer = customers.find(customer => customer.cpf === cpf);
-
-    if(!customer) {
-        return response.status(400).json({ error : "Customer Not Found"});
-    }
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) =>{
+    const { customer } = request;
 
     return response.json(customer.statement);
 });
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+    
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+
 
 
 
