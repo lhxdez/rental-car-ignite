f67b3a58859cd8ae4dbfb3c6692b69f84e3d237d
Lucas Gomes <jatinhahxdez@gmail.com>
2022-05-25 22:34:56 -0300

    feat: add delete account and balance

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { type } = require('express/lib/response');
+const { type, get } = require('express/lib/response');
 const { v4: uuidv4 } = require("uuid")
 
 const app = express()
@@ -112,6 +112,37 @@ app.get("/statement/date",verifyIfExistsAccountCPF, (request, response) =>{
     );
 
     return response.json(statement);
+});
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
 });
 
 app.listen(3333);
