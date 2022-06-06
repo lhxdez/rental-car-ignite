559c32b839861428983b7ddf97d6b272894b5c89
Lucas da Silveira Gomes <94697673+LucasGomes98@users.noreply.github.com>
2022-06-06 16:40:07 -0300

    feat: add new categories routes and category type

--- Chapter2/Part2/src/server.ts
+++ Chapter2/Part2/src/server.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    return res.json( { message : " Hello World! " } )
-});
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
+
+
+
+// app.get("/", (req, res) => {
+//     return res.json( { message : " Hello World! " } )
+// });
+
+// app.post("/courses", (req, res) => { 
+//     const { name } = req.body;
+
+//     return res.json({ name });
+// })
