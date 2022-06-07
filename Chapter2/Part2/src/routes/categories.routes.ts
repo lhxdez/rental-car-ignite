6f720d75049da303b320e45f628e7aca81216e3d
Lucas Gomes <jatinhahxdez@gmail.com>
2022-06-07 20:15:43 -0300

    feat: add new category repo

--- Chapter2/Part2/src/routes/categories.routes.ts
+++ Chapter2/Part2/src/routes/categories.routes.ts
@@ -1,24 +1,16 @@
 import { Router } from "express";
 import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/post-category", (req, res) => {
     const { name, description } = req.body;
 
-    const category = new Category();
-    
-    Object.assign(category, {
-        name,
-        description,
-        created_at: new Date()
-    });
+    categoriesRepository.create({name, description});
 
-    categories.push(category);
-
-    return res.status(201).json({ category });
+    return res.status(201).send();
 });
 
 export { categoriesRoutes };
