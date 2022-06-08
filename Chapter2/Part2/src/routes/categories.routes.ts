4db857ab69443c5a431d64e36ab2bf2fdb142416
Lucas Gomes <jatinhahxdez@gmail.com>
2022-06-07 22:00:35 -0300

    feat: add new validations

--- Chapter2/Part2/src/routes/categories.routes.ts
+++ Chapter2/Part2/src/routes/categories.routes.ts
@@ -8,9 +8,21 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/post-category", (req, res) => {
     const { name, description } = req.body;
 
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+    if(categoryAlreadyExists) {
+        return res.status(400).json({ error : "Category already exists!" });
+    }
+
     categoriesRepository.create({name, description});
 
     return res.status(201).send();
+});
+
+categoriesRoutes.get("/list-categories", (req, res) => {
+    const lists = categoriesRepository.list();
+
+    return res.json(lists);
 });
 
 export { categoriesRoutes };
