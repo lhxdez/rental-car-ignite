7bcc3812bc02d91cf4b305f118be09598f998cc5
Lucas Gomes <jatinhahxdez@gmail.com>
2022-06-11 12:08:21 -0300

    feat: add routes index file

--- Chapter2/Part2/src/server.ts
+++ Chapter2/Part2/src/server.ts
@@ -1,24 +1,10 @@
 import express from "express";
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
-
-
-
-// app.get("/", (req, res) => {
-//     return res.json( { message : " Hello World! " } )
-// });
-
-// app.post("/courses", (req, res) => { 
-//     const { name } = req.body;
-
-//     return res.json({ name });
-// })
