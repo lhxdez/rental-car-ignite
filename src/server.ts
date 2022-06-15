1a9a0a8b132f9affd47297299803e6354fda4229
Lucas da Silveira Gomes <94697673+LucasGomes98@users.noreply.github.com>
2022-06-15 15:26:21 -0300

    feat: add docker database config

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
-import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import { router } from './routes';
-import swaggerFile from './swagger.json';
+import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import { router } from './routes'
+import swaggerFile from './swagger.json'
 
-const app = express();
+import './database'
 
-app.use(express.json());
+const app = express()
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(express.json())
 
-app.use(router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
-app.listen(3333, () => console.log('Server is running!'));
+app.use(router)
+
+app.listen(3333, () => console.log('Server is running!'))
