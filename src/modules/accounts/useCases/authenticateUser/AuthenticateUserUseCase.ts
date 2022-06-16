959c49819a85008271481f1b36dd62416b46a299
Lucas da Silveira Gomes <94697673+LucasGomes98@users.noreply.github.com>
2022-06-16 16:40:32 -0300

    feat: add jwt auth token

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,53 @@
+import { IUsersRepository } from '../../../../modules/accounts/repositories/IUsersRepository'
+import { inject, injectable } from 'tsyringe'
+import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+
+interface IRequest {
+  email: string
+  password: string
+}
+
+interface IResponse {
+  user: {
+    email: string
+    name: string
+  }
+  token: string
+}
+
+@injectable()
+export class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private userRepository: IUsersRepository,
+  ) {}
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.userRepository.findByEmail(email)
+
+    if (!user) {
+      throw new Error('Email or password incorrect!')
+    }
+
+    const passwordMatch = await compare(password, user.password)
+
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect!')
+    }
+
+    const token = sign({}, '8c2efd7ca4af720addf5f6ca15fe300d', {
+      subject: user.id,
+      expiresIn: '1d',
+    })
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    }
+
+    return tokenReturn
+  }
+}
