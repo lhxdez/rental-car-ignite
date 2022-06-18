3839dfd36fce915e447ff32e15a93e3437e5359c
Lucas Gomes <jatinhahxdez@gmail.com>
2022-06-18 11:28:01 -0300

    feat: add delete avaatar

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,23 +1,28 @@
-import { IUsersRepository } from "../../../../modules/accounts/repositories/IUsersRepository";
-import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from '../../../../modules/accounts/repositories/IUsersRepository'
+import { deleteFile } from 'utils/file'
+import { inject, injectable } from 'tsyringe'
 
 interface IRequest {
-    user_id: string;
-    avatar_file: string;
+  user_id: string
+  avatar_file: string
 }
 
 @injectable()
 export class UpdateUserAvatarUseCase {
-    constructor(
-        @inject('UsersRepository')
-        private usersRepository: IUsersRepository
-    ){}
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
 
-    async execute({user_id, avatar_file} : IRequest): Promise<void> {
-        const user = await this.usersRepository.findById(user_id)
+  async execute({ user_id, avatar_file }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findById(user_id)
 
-        user.avatar = avatar_file
-
-        await this.usersRepository.create(user)
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`)
     }
+
+    user.avatar = avatar_file
+
+    await this.usersRepository.create(user)
+  }
 }
