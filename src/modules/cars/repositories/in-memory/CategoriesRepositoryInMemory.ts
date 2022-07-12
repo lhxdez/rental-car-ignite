f6dea3a4a32fe82bb5020e2fa3213ffda40dadf4
Lucas Gomes <lucas.gomes@startse.com>
2022-07-12 18:40:33 -0300

    feat: add create category test and fake repo and jest config

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,27 @@
+import { Category } from '../../entities/Category'
+import { ICategoryRepository, ICreateCategoryDTO } from '../ICategoryRepository'
+
+export class CategoriesRepositoryInMemory implements ICategoryRepository {
+  categories: Category[] = []
+
+  async findByName(name: string): Promise<Category> {
+    const category = this.categories.find(category => category.name === name)
+
+    return category
+  }
+  async list(): Promise<Category[]> {
+    const all = this.categories
+
+    return all
+  }
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = new Category()
+
+    Object.assign(category, {
+      name,
+      description,
+    })
+
+    this.categories.push(category)
+  }
+}
