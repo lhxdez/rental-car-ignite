8172c369b799d99a49585b298da0663d42bd6747
Lucas Gomes <jatinhahxdez@gmail.com>
2022-08-01 19:44:10 -0300

    feat: add new create specifications cars migration

---
+++ src/shared/infra/typeorm/migrations/1659393047700-CreateSpecificationsCars.ts
@@ -0,0 +1,66 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm'
+
+export class CreateSpecificationsCars1659393047700
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'specifications_cars',
+        columns: [
+          {
+            name: 'car_id',
+            type: 'uuid',
+          },
+          {
+            name: 'specification_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    )
+
+    await queryRunner.createForeignKeys('specifications_cars', [
+      new TableForeignKey({
+        name: 'FKSpecificationCar',
+        referencedColumnNames: ['id'],
+        referencedTableName: 'specifications',
+        columnNames: ['specification_id'],
+        onDelete: 'SET NULL',
+        onUpdate: 'SET NULL',
+      }),
+      new TableForeignKey({
+        name: 'FKCarSpecification',
+        referencedColumnNames: ['id'],
+        referencedTableName: 'cars',
+        columnNames: ['car_id'],
+        onDelete: 'SET NULL',
+        onUpdate: 'SET NULL',
+      }),
+    ])
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      'specifications_cars',
+      'FKCarSpecification',
+    )
+
+    await queryRunner.dropForeignKey(
+      'specifications_cars',
+      'FKSpecificationCar',
+    )
+
+    await queryRunner.dropTable('specifications_cars')
+  }
+}
