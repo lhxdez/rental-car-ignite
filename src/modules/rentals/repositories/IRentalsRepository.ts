b3de1d473487bb018c67ce1664d25234cdcb5ac4
Lucas Gomes <jatinhahxdez@gmail.com>
2022-08-02 20:19:06 -0300

    feat: add rentals module

---
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -0,0 +1,6 @@
+import { Rental } from '../infra/typeorm/entities/Rental'
+
+export interface IRentalsRepository {
+  findOpenRentalByCar(car_id: string): Promise<Rental>
+  findOpenRentalByUser(user_id: string): Promise<Rental>
+}
