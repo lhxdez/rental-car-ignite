c39fe5e706921700f1356a77e825a4e6720f3f9d
Lucas Gomes <jatinhahxdez@gmail.com>
2022-08-08 20:54:58 -0300

    feat: add list rentals use case

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -50,4 +50,12 @@ export class RentalsRepository implements IRentalsRepository {
     const rental = await this.repository.findOne(id)
     return rental
   }
+
+  async findByUser(user_id: string): Promise<Rental[]> {
+    const rental = await this.repository.find({
+      user_id,
+    })
+
+    return rental
+  }
 }
