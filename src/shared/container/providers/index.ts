12b75bd1ca09f8871d1dffe3dd9ba680f6606009
Lucas Gomes <jatinhahxdez@gmail.com>
2022-08-14 19:15:54 -0300

    feat: add local storage provider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,8 +1,15 @@
 import { container } from 'tsyringe'
 import { IMailProvider } from './MailProvider/IMailProvider'
 import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider'
+import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider'
+import { IStorageProvider } from './StorageProvider/IStorageProvider'
 
 container.registerInstance<IMailProvider>(
   'EtherealMailProvider',
   new EtherealMailProvider(),
+)
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  LocalStorageProvider,
 )
